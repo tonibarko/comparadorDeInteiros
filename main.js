abe0becadee1b640aabaee607f29db10cfccaf1b
Antonio Barkokebas <toni_barko@hotmail.com>
2023-04-25 20:29:17 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('formulario')
+
+form.addEventListener('submit', e=> {
+    e.preventDefault();
+
+    const identificaA = document.getElementById('input-A')
+    const identificaB = document.getElementById('input-B')
+
+    if (identificaA.value < identificaB.value){
+        document.querySelector('.sucess-message').style.display='block'
+        identificaA.value = '';
+        identificaB.value = '';
+
+    }else {
+        document.querySelector('.error-message').style.display = 'block'
+    }
+
+    identificaA.addEventListener('click', e=>{
+        document.location.reload(true)
+    })
+    
+})
+
+
